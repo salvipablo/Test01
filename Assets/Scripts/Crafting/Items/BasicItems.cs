987787ee09490b1e5e39b1823706fc1c998dce52
psalvi <salvipablo@gmail.com>
2022-07-25 15:42:29 -0300

    Basic crafting
    Resolve: T#00006

---
+++ Assets/Scripts/Crafting/Items/BasicItems.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicItems : MonoBehaviour
+{
+    public void craftTable()
+    {
+        Debug.Log("Mesa de crafteo");
+    }
+
+    public void woodenShovel()  // Pala de madera
+    {
+        string[] items = { "Wood Plank", "Wood Sticks" };
+        int[] quantityItems = { 3, 2 };
+        
+        bool itemsExists = canICreateItem(items, quantityItems);
+
+        if (itemsExists)
+        {
+            Item itemInsert = new Item(3, "Wooden Shovel", "Shovel_Wooden", 1);
+
+            itemsExists = false;
+
+            foreach (Item item in PlayerManager.inventory)
+            {
+                if (item.name.Equals("Wooden Shovel"))
+                {
+                    item.quantity += itemInsert.quantity;
+                    itemsExists = true;
+                }
+            }
+
+            if (!itemsExists) PlayerManager.inventory.Add(itemInsert);
+        }
+        else
+        {
+            Debug.Log("No tiene los elementos para crear una pala de madera");
+        }
+    }
+
+    public void woodenAx()
+    {
+        Debug.Log("Hacha de madera");
+    }
+
+    public void woodenPickaxe()
+    {
+        Debug.Log("Pico de madera");
+    }
+
+    public bool canICreateItem(string[] necessaryItems, int[] quantitiesNeeded)
+    {
+        bool resourceFound = false;
+
+        for (int i = 0; i < necessaryItems.Length; i++)
+        {
+            foreach (Item itemInv in PlayerManager.inventory)
+            {
+                if (itemInv.name.Equals(necessaryItems[i]) && itemInv.quantity > 0)
+                {
+                    itemInv.quantity -= quantitiesNeeded[i];
+                    if (itemInv.quantity == 0) PlayerManager.inventory.Remove(itemInv);
+                    resourceFound = true;
+                    break;
+                }
+            }
+        }
+
+        return resourceFound;
+    }
+}
--- Assets/Scripts/Crafting/Materials/Basic.cs
+++ Assets/Scripts/Crafting/Materials/Basic.cs
@@ -6,11 +6,83 @@ public class Basic : MonoBehaviour
 {
     public void craftingWood()
     {
-        Debug.Log("Tablas de Madera");
+        bool itemExists = false;
+
+        // Tengo que tener 1 de Common Wood
+        foreach (Item item in PlayerManager.inventory)
+        {
+            if (item.name.Equals("Common Wood") && item.quantity > 0)
+            {
+                itemExists = true;
+                item.quantity -= 1;
+                if (item.quantity == 0) PlayerManager.inventory.Remove(item);
+                break;
+            }
+
+            
+        }
+
+        if (itemExists)
+        {
+            Item itemInsert = new Item(3, "Wood Plank", "Plank_Wood", 10);
+
+            itemExists = false;
+
+            foreach (Item item in PlayerManager.inventory)
+            {
+                if (item.name.Equals("Wood Plank"))
+                {
+                    item.quantity += itemInsert.quantity;
+                    itemExists = true;
+                }
+            }
+
+            if (!itemExists) PlayerManager.inventory.Add(itemInsert);
+        }
+        else
+        {
+            Debug.Log("No tiene madera comun para crear tabla de madera");
+        }
     }
 
     public void craftingSticks()
     {
-        Debug.Log("Palitos");
+        bool itemExists = false;
+
+        // Tengo que tener 1 de Common Wood
+        foreach (Item item in PlayerManager.inventory)
+        {
+            if (item.name.Equals("Wood Plank") && item.quantity > 0)
+            {
+                itemExists = true;
+                item.quantity -= 1;
+                if (item.quantity == 0) PlayerManager.inventory.Remove(item);
+                break;
+            }
+
+
+        }
+
+        if (itemExists)
+        {
+            Item itemInsert = new Item(3, "Wood Sticks", "Sticks_Wood", 3);
+
+            itemExists = false;
+
+            foreach (Item item in PlayerManager.inventory)
+            {
+                if (item.name.Equals("Wood Sticks"))
+                {
+                    item.quantity += itemInsert.quantity;
+                    itemExists = true;
+                }
+            }
+
+            if (!itemExists) PlayerManager.inventory.Add(itemInsert);
+        }
+        else
+        {
+            Debug.Log("No tiene tablas de madera para crear palitos");
+        }
     }
 }
